615dc8d858df2ebda91f8117072b0e629b532653
Mohamedking451 <86286863+Mohamedking451@users.noreply.github.com>
2021-12-15 12:46:21 +0300

    Update Server435.py

--- Server435.py
+++ Server435.py
@@ -1,4 +1,8 @@
 import requests
-r = requests.post("https://discord.com/api/v9/channels/920546145623740426/messages", headers = {"Authorization" : "OTIwNjA3NDEwOTk2MDE1MTY0.Ybm3FQ.aPXLnbw1Z88jJhE8cHGnVepUML4"}, data = {"content" : "!d bump"})
-print(r.json())
+import time
+while True:
+	r = requests.post("https://discord.com/api/v9/channels/920546145623740426/messages", headers = {"Authorization" : "OTE4NDA2MzA1NDExMTI5MzY0.Ybm48Q.6TnInPz7f6UdecKv2WHhjPbYehs"}, data = {"content" : "!d bum"})
+	print(r.json())
+	time.sleep((60 * 60) * 2)
+
 
