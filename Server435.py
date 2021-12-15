892001a229ad8ce07b4e3868e5e8321343f6f611
Mohamedking451 <86286863+Mohamedking451@users.noreply.github.com>
2021-12-15 12:05:29 +0300

    Update Server435.py

--- Server435.py
+++ Server435.py
@@ -1,14 +1,3 @@
-import websockets
-import asyncio
-print("Waiting for clients")
-async def echo(websocket):
-    async for message in websocket:
-        print(message)
-        while True:
-        	await websocket.send(input("Send message"))
-
-async def main():
-    async with websockets.serve(echo, "localhost", 8765):
-        await asyncio.Future()  # run forever
-
-asyncio.run(main())
+import requests
+re = requests.post("https://discord.com/api/v9/auth/login", json = {"email" : "helly657@gmail.com", "password" : "deeq1012"})
+r = requests.post("https://discord.com/api/v9/channels/920546145623740426/messages", headers = {"Authorization" : re.json()["token"]}, data = {"content" : "!d bump"})
