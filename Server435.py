4d914ff9fd9044f7d25198778af3a1b8bb07ed1d
Mohamedking451 <86286863+Mohamedking451@users.noreply.github.com>
2021-12-15 21:37:42 +0300

    Update Server435.py

--- Server435.py
+++ Server435.py
@@ -1,8 +1,8 @@
 import requests
 import time
+headers = {"Authorization" : "OTIwNzQzMjAxNjY3NzA2ODkw.YbozHw.BlWXBitH4kx7gfi-HetCnR7gh1A"}
+data1 = {"content" : "!d bump"}
 while True:
-	r = requests.post("https://discord.com/api/v9/channels/920546145623740426/messages", headers = {"Authorization" : "OTE4NDA2MzA1NDExMTI5MzY0.Ybm6hA.ywbmSW3UVmr_uS-Ium8ZelLt1n0"}, data = {"content" : "!d bump"})
-	print(r.json())
+	r = requests.post("https://discord.com/api/v9/channels/920546145623740426/messages", headers = headers, data = data1)
+	print(r.status_code)
 	time.sleep((60 * 60) * 2)
-
-
