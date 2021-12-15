9ac76afa9e3a8531a71e475e1d029e8ba330e275
Mohamedking451 <86286863+Mohamedking451@users.noreply.github.com>
2021-12-15 12:33:02 +0300

    Update Server435.py

--- Server435.py
+++ Server435.py
@@ -1,5 +1,5 @@
 import requests
-re = requests.post("https://discord.com/api/v9/auth/login", json = {"email" : "helly657@gmail.com", "password" : "deeq1012"})
-r = requests.post("https://discord.com/api/v9/channels/920546145623740426/messages", headers = {"Authorization" : "OTE4NTMwNTk5NDMyNjQ2Njk2.YbmxuA._GdBmXcPE5g1xq5Al26JL2KHKw0"}, data = {"content" : "!d bump"})
-print(re.json())
+re = requests.post("https://discord.com/api/v9/auth/login", json = {"email" : "lmoa62626@gmail.com", "password" : "deeq1012"})
+r = requests.post("https://discord.com/api/v9/channels/920546145623740426/messages", headers = {"Authorization" : re.json()["token"]}, data = {"content" : "lol"})
 print(r.json())
+
